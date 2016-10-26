58f6ac51ada6b2a3da1181207dfa28c63be04cf5
Ильдар <>
2016-10-27 00:06:11 +0300

    Добавлены тестовые сообщения с вложениями на ФТП

--- lbss-db/import-data.sh
+++ lbss-db/import-data.sh
@@ -2,3 +2,5 @@
 mongoimport --db lbss-members --collection SmevMember --file /data/preset/SmevMember.json 
 mongoimport --db lbss-messages --collection RequestMessage --file /data/preset/RequestMessage.json
 mongoimport --db lbss-messages --collection ResponseMessage --file /data/preset/ResponseMessage.json
+mongoimport --db lbss-messages --collection SmevAsyncProcessingMessage --file /data/preset/StatusMessage.json
+mongoimport --db lbss-messages --collection FTPUser --file /data/preset/FTPUser.json
